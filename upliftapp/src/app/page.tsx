6c57f4e1d41ca7ec79be788fee299c71cce5e3f1
Scott Shields <swshiel@clemson.edu>
2025-03-08 01:04:09 -0500

    Layout changes

--- upliftapp/src/app/page.tsx
+++ upliftapp/src/app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
     <div className="p-4">
-      <h1 className="text-3xl font-bold">Welcome to UpliftApp</h1>
+      <h1 className="text-3xl font-bold">Welcome to Uplift!</h1>
       <p className="text-lg text-gray-600">
-        Connect with local non-profits, charities, and events!
+        We're here to help you connect with local non-profits, charities, and events!
+        Follow organizations you like, RSVP for events you want to attend, track your
+        events in the calendar, and message organizations with questions you have!
       </p>
     </div>
   );
