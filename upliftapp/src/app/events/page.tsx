f3a04e6df32872b7d7ca7cb5f272e86ee234805a
Scott Shields <swshiel@clemson.edu>
2025-03-08 12:00:35 -0500

    Events to display all

--- upliftapp/src/app/events/page.tsx
+++ upliftapp/src/app/events/page.tsx
@@ -1,16 +1,32 @@
 "use client";
 
+import { useEvents } from "@/app/context/eventsContext";
 import EventCard from "@/components/eventCard";
 
 export default function Events() {
+    const { events } = useEvents();
+
     return (
-    <div className="p-4">
-        <h1 className="text-2xl font-bold">Upcoming Events</h1>
-        <EventCard 
-        name="Community Cleanup"
-        description="Join us to clean up the park this Saturday!"
-        date="March 15, 2025"
-        />
-    </div>
+        <div className="p-4">
+            <h1 className="text-2xl font-bold">Upcoming Events</h1>
+            
+            {events.length === 0 ? (
+                <p className="mt-4 text-gray-600">No upcoming events.</p>
+            ) : (
+                <ul className="mt-4">
+                    {events.map((event) => {
+                        return (
+                            <li key={event.id} className="mb-4">
+                                <EventCard 
+                                    name={event.name}
+                                    description={`Hosted by ${event.organizationName}`}
+                                    date={event.date}
+                                />
+                            </li>
+                        );
+                    })}
+                </ul>
+            )}
+        </div>
     );
 }
