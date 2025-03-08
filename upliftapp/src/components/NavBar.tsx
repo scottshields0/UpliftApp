00315e3f5b450410eab8af83dce9e64194ddeadf
Scott Shields <swshiel@clemson.edu>
2025-03-08 01:08:19 -0500

    Updated NavBar

--- upliftapp/src/components/NavBar.tsx
+++ upliftapp/src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavBar() {
     return (
-    <nav className="mb-4 justify-center items-center space-x-4 p-4 bg-gray-100 shadow-md">
+    <nav className="mb-4 justify-center items-center w-full space-x-4 p-4 bg-gray-100 shadow-md">
         <Link href="/" className="text-blue-500">Home</Link>
         <Link href="/organizations" className="text-blue-500">Organizations</Link>
         <Link href="/events" className="text-blue-500">Events</Link>
